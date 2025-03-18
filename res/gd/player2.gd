60139dd0caf19bec3e2c17a0e7cf06acd95bdcfa
钊 李 <lz448992056@Outlook.com>
2025-03-19 05:30:13 +0800

    在动作层面上也进行了转换

--- res/gd/player2.gd
+++ res/gd/player2.gd
@@ -1,1 +1,8 @@
 extends "res://res/gd/player.gd"
+func _physics_process(delta: float) -> void:
+	direction=0
+	if action:
+		direction =action["left"]-action["right"]
+	velocity.x=direction*speed
+	velocity.y=0
+	move_and_collide(velocity*delta)
